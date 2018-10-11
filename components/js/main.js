8b0d9d948b078ba3a41d152f7489cef6087575de
Jingyi21 <qianwumeng_2cyndi@qq.com>
2018-10-11 15:59:06 +0800

    Update v0.1

--- components/js/main.js
+++ components/js/main.js
@@ -1,1 +1,19 @@
-console.log("finished");
+import $ from 'jquery';
+import css from '../css/style.css';
+
+$(document).scroll(function() {
+    let sTop = $(this).scrollTop();
+    let ft = $("#first-section").offset().top;
+    
+    if (sTop > ft) {
+        $('.header-nav').css({'background-color':'#fff','box-shadow':'0 2px 12px rgba(0,0,0,.18)'});
+        $('.header-logo-name').css('color','rgba(0,0,0,.54)');
+        $('.header-nav-list li a').css('color','rgba(0,0,0,.54)');
+    }
+    if (sTop < ft) {
+        $('.header-nav').css({'background-color':'transparent','box-shadow':'none'});
+        $('.header-logo-name').css('color','#fff');
+        $('.header-nav-list li a').css('color','#fff');
+    }
+});
+
