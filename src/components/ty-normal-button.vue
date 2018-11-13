09d57fbf64f526e569c7be794d2d04caf578c7b7
Miya <qianwumeng_2cyndi@qq.com>
2018-11-14 00:28:43 +0800

    Update src/components/ty-normal-button.vue

---
+++ src/components/ty-normal-button.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <a :href="links" target="_blank">
+            <button>
+                <i class="fa" :class="icon"> {{ message }}</i>
+            </button>
+        </a>
+    </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: ['links','icon','message'],
+  data () {
+    return {
+    }
+  },
+  watch: {},
+  computed: {},
+  methods: {},
+  created () {},
+  mounted () {}
+}
+</script>
+<style scoped>
+</style>
