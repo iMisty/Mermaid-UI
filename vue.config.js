4bc7bc9a0d4f31108facff29a47148e229745c4d
Miya <qianwumeng_2cyndi@qq.com>
2019-02-08 23:31:13 +0800

    Create vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  baseUrl: process.env.NODE_ENV === 'production' ? '/' : '/',
+
+  devServer: {
+    proxy: {
+        '/api': {
+            target: 'http://192.168.0.88:8080',   // 需要请求的地址
+            changeOrigin: false,  // 是否跨域
+            pathRewrite: {
+                '^/api': '/'  // 替换target中的请求地址，也就是说，在请求的时候，url用'/proxy'代替'http://ip.taobao.com'
+            }
+        }
+
+    },  // 配置多个代理
+  }
+};
