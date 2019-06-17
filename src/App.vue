02623551978b3abdd529c350aee7db8604599733
Miya <qianwumeng_2cyndi@qq.com>
2019-06-17 22:43:19 +0800

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+
+</template>
+
+<script>
+import './assets/ykstyle.less';
+export default {
+  components: {},
+  props: [],
+  data () {
+    return {
+    }
+  },
+  watch: {},
+  computed: {},
+  methods: {},
+  created () {},
+  mounted () {}
+}
+</script>
+<style lang="less" scoped>
+</style>
