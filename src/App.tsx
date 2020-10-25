1c853ed904f8aa22759fe81b0698a031a1f86283
Miya <qianwumeng_2cyndi@qq.com>
2020-10-25 21:47:28 +0800

    Fix: Vue → TSX

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+/*
+ * @Author: Miya
+ * @Version: 1.0
+ * @Date: 2020-09-12 17:57:06
+ * @LastEditors: Miya
+ * @LastEditTime: 2020-10-25 21:36:49
+ * @Description: file content
+ * @FilePath: /Mermaid-UI/src/App.tsx
+ */
+import { Component, Vue } from 'vue-property-decorator';
+@Component({})
+export default class App extends Vue {
+  private render() {
+    return <div id='app'>Coding</div>;
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,12 @@
  * @Version: 1.0
  * @Date: 2020-09-12 17:57:06
  * @LastEditors: Miya
- * @LastEditTime: 2020-09-12 22:45:03
+ * @LastEditTime: 2020-10-25 21:36:57
  * @Description: file content
  * @FilePath: /Mermaid-UI/src/main.ts
  */
 import Vue from 'vue';
-import App from './App.vue';
+import App from './App';
 import router from './router';
 
 Vue.config.productionTip = false;
