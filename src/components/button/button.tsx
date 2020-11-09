723012d2f61443ab41755986ceedc6b942da95cd
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2020-11-09 18:05:28 +0800

    Update button.tsx

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,36 +1,39 @@
 /*
  * @Author: Miya
  * @Date: 2020-09-03 17:08:06
- * @LastEditTime: 2020-09-13 22:11:25
+ * @LastEditTime: 2020-11-09 17:57:45
  * @LastEditors: Miya
  * @Description: Button component in Mermaid UI
- * @FilePath: /Mermaid-UI/src/components/button/button.tsx
+ * @FilePath: \Mermaid-UI\src\components\button\button.tsx
  * @Version: 0.2
  */
-import { Component, Vue, Prop, Emit } from 'vue-property-decorator';
+import { Component, Vue, Prop, Emit } from "vue-property-decorator";
 @Component({})
 export default class MermaidButton extends Vue {
-  // Extra class name
-  // 额外类名
-  @Prop({ default: '' })
-  private className?: string;
-
   // Button Color
   // 按钮颜色
-  @Prop({ default: 'primary' })
+  @Prop({ default: "primary" })
   private color!: string;
+
+  // Button Type
+  @Prop({ default: "round" })
+  private type!: string;
+
+  // Button Size
+  @Prop({ default: "regular" })
+  private size!: string;
 
   // Click Event
   // 点击事件
-  @Emit('clickevent')
+  @Emit("clickevent")
   public handleClick() {
-    console.log('click button');
+    console.log("click button");
   }
 
   private render() {
     return (
       <button
-        class={`mermaid__btn mermaid__btn--${this.color} ${this.className}`}
+        class={`mermaid__button mermaid__button--${this.color} mermaid__button--type-${this.type}-${this.color}`}
         onClick={this.handleClick}
       >
         {this.$slots.default}
