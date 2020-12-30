87dcc33a7965e8b9b232a183453d250afe5ed66f
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2020-12-30 18:28:25 +0800

    WIP: leftlist

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -11,12 +11,35 @@ export default class Home extends Vue {
           style="width: 16rem;height: 100vh;background-color: #fff;padding: 0.5rem;"
         >
           <section class="mermaid__leftlist">
-            <header class="mermaid__leftlist--title">
+            <header class="mermaid__leftlist--title mermaid__leftlist--list--item">
               <i class="fa fa-plane"></i>
               <p class="text">111111111111</p>
             </header>
             <article class="mermaid__leftlist--list">
-              
+              <section class="mermaid__leftlist--list--item">
+                <i class="fa fa-plane"></i>
+                <p class="text">111111111111</p>
+              </section>
+              <section class="mermaid__leftlist--list--item">
+                <i class="fa fa-plane"></i>
+                <p class="text">2222222222222</p>
+              </section>
+              <section class="mermaid__leftlist--list--item">
+                <i class="fa fa-plane"></i>
+                <p class="text">3333333333333</p>
+              </section>
+              <section class="mermaid__leftlist--list--item">
+                <i class="fa fa-plane"></i>
+                <p class="text">44444444444</p>
+              </section>
+              <section class="mermaid__leftlist--list--item">
+                <i class="fa fa-plane"></i>
+                <p class="text">5555555555555</p>
+              </section>
+              <section class="mermaid__leftlist--list--item">
+                <i class="fa fa-plane"></i>
+                <p class="text">66666666666666s</p>
+              </section>
             </article>
           </section>
         </div>
