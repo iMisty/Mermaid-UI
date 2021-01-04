9d2be267d71ae04807b3663fc73f4ac6af365cfb
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2021-01-04 16:08:58 +0800

    Update Variable

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: Miya
  * @Date: 2020-12-29 15:04:19
- * @LastEditTime: 2020-12-29 15:43:09
+ * @LastEditTime: 2021-01-04 12:02:36
  * @LastEditors: Miya
  * @Description:
  * @FilePath: \Single-Search-APIc:\Users\Platinum Prism\Documents\GitHub\Mermaid-UI\src\main.ts
@@ -11,7 +11,7 @@ import Vue from 'vue';
 import App from './App';
 import router from './router';
 import store from './store';
-import './style/style.less';
+import './style/normalize.less';
 
 Vue.config.productionTip = false;
 
