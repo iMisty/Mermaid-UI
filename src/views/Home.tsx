2d7dcac1aa89326af8afaa6d1f777c8393f5292d
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2021-01-04 16:09:05 +0800

    Update Home.tsx

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,16 +1,25 @@
 import { Component, Vue } from 'vue-property-decorator';
 import 'font-awesome/css/font-awesome.min.css';
 import LeftListItem from '../components/leftlist/leftlistitem';
+// import Card from '../components/card/card';
+// import Model from '../components/model/model';
+import mButton from '../components/button/button';
 
 @Component({
   components: {
-    leftlistitem: LeftListItem
+    leftlistitem: LeftListItem,
+    // Card
+    // Model,
+    'm-button': mButton
   }
 })
 export default class Home extends Vue {
   private render() {
     return (
       <div class="home" style="background-color: #f3f4f5;">
+        <m-button color="info"><p>test button</p></m-button>
+        <m-button color="success"><p>test button2</p></m-button>
+
         <div
           class="home__left"
           style="width: 16rem;height: 100vh;background-color: #fff;padding: 0.5rem;"
