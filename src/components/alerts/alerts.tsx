5caf438af342bbea054e02332fab0b035aaeccff
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2021-01-07 18:17:38 +0800

    Create alerts.tsx

---
+++ src/components/alerts/alerts.tsx
@@ -0,0 +1,35 @@
+import { Component, Vue, Emit } from 'vue-property-decorator';
+import './alerts.less';
+
+@Component({})
+export default class Alert extends Vue {
+  private text = 'This is a alert';
+
+  private style = 'primary';
+
+  private isShow = true;
+
+  @Emit('close')
+  private close() {
+    console.log('close');
+    return (this.isShow = false);
+  }
+
+  private render() {
+    return (
+      <transition name="fade" appear appear-class="fade-enter-active">
+        {this.isShow ? (
+          <div class={`mmui__alert ${this.style}`}>
+            <section class="mmui__alert--text">{this.text}</section>
+            <section
+              class="mmui__alert--close"
+              onClick={() => this.close()}
+            >×</section>
+          </div>
+        ) : (
+          ''
+        )}
+      </transition>
+    );
+  }
+}
