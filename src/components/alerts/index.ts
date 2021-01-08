7eaa2e3cb87eec309b3e51fd208b214212be7626
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2021-01-08 18:17:47 +0800

    Create index.ts

---
+++ src/components/alerts/index.ts
@@ -0,0 +1,9 @@
+/*
+ * @Author: Miya
+ * @Date: 2021-01-11 18:06:23
+ * @LastEditTime: 2021-01-11 18:06:23
+ * @LastEditors: Miya
+ * @Description: 
+ * @FilePath: \Single-Search-APIc:\Users\Platinum Prism\Documents\GitHub\Mermaid-UI\src\components\alerts\index.ts
+ * @Version: 
+ */
