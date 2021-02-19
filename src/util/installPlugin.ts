93dc0c9259c3f48118933a0d231e1e967d6c9b3d
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2021-02-19 17:38:21 +0800

    Remove

---
+++ src/util/installPlugin.ts
@@ -0,0 +1,33 @@
+/*
+ * @Author: Miya
+ * @Date: 2021-02-18 17:59:09
+ * @LastEditTime: 2021-02-18 18:09:30
+ * @LastEditors: Miya
+ * @Description: 将组件安装为Vue3组件插件工具函数
+ * @FilePath: \Mermaid-UI\src\util\installPlugin.ts
+ * @Version: 1.0
+ */
+
+// import component from '*.vue';
+// import { App, Plugin } from 'vue';
+
+
+// /**
+//  * @description 组件安装为插件工具函数
+//  * @param Component 组件名称
+//  * @param plugins 插件名称
+//  */
+// export function installPlugin<T extends { name: string }>(
+//   Component: T,
+//   plugins?: Plugin[]
+// ) {
+//   return {
+//     ...component,
+//     install(app: App) {
+//       app.component(Component.name, Component);
+//       if (plugins) {
+//         plugins.forEach(app.use);
+//       }
+//     },
+//   };
+// }
