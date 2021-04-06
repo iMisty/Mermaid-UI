7c035058481f26fec7038421411b9e9c983c59a7
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2021-04-06 23:42:16 +0800

    Reinit

--- example/layout/LayoutAdmin.tsx
+++
@@ -1,8 +0,0 @@
-import { Component, Vue } from 'vue-property-decorator';
-
-@Component({})
-export default class LayoutAdmin extends Vue {
-  private render() {
-    return <div class="layout__admin"></div>;
-  }
-}
--- vue.config.ts
+++ vue.config.ts
@@ -8,26 +8,25 @@
  * @Version: 2.0
  */
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path');
 
 module.exports = {
   pages: {
     index: {
       entry: 'example/main.ts',
-      template: 'public/index.html',
-      filename: 'index.html'
-    }
+      template: 'index.html',
+      filename: 'index.html',
+    },
   },
-  chainWebpack: config => {
+  chainWebpack: (config) => {
     config.module
       .rule('js')
       .include.add(path.resolve(__dirname, 'packages'))
       .end()
       .use('babel')
       .loader('babel-loader')
-      .tap(options => {
+      .tap((options) => {
         return options;
       });
-  }
+  },
 };
