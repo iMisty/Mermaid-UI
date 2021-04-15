0be0ff81c68048c0e7173075c256255b23224105
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2021-04-16 00:33:35 +0800

    init

--- example/view/Home.tsx
+++ example/view/Home.tsx
@@ -1,12 +1,21 @@
 import { defineComponent, reactive } from 'vue';
+import MMUISwitch from '../../package/switch/switch';
+
 const data = reactive({});
 
 const Home = defineComponent({
+  components: {
+    'mmui-switch': MMUISwitch,
+  },
   setup(props, ctx) {
     return { data };
   },
   render() {
-    return <div class="home">2222</div>;
+    return (
+      <div class="home">
+        <mmui-switch></mmui-switch>
+      </div>
+    );
   },
 });
 export default Home;
