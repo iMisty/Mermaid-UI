936fd2a3c5d5df10a1f96ed00a09a8ddb49b1a8f
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2021-06-16 05:24:27 +0800

    Update: Button

--- package/button/button.tsx
+++ package/button/button.tsx
@@ -1,37 +1,61 @@
 /*
  * @Author: Miya
  * @Date: 2020-09-03 17:08:06
- * @LastEditTime: 2021-04-21 10:16:54
+ * @LastEditTime: 2021-06-16 05:21:37
  * @LastEditors: Miya
  * @Description: Button Component
  * @FilePath: \Mermaid-UI\package\button\button.tsx
  * @Version: 0.5
  */
 import { defineComponent, reactive, renderSlot } from 'vue';
+import './button.less';
 
 const data = reactive({});
 
-const MMUI_Button = defineComponent({
-  setup(prop, ctx) {
+const handleClick = () => {
+  console.log('click button');
+};
+
+const MermaidUIButton = defineComponent({
+  name: 'MermaidUIButton',
+  props: {
+    color: {
+      type: String,
+      require: true,
+      default: 'primary',
+    },
+    type: {
+      type: String,
+      require: true,
+      default: 'round',
+    },
+    size: {
+      type: String,
+      require: true,
+      default: 'regular',
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  emits: ['clickevent'],
+  setup(props, ctx) {
     data;
     return { data };
   },
-
   render() {
     return (
       <button
-        class="mmui__button"
-        // :class="[
-        //   getColor,
-        //   getType,
-        //   `mmui__button--size-${size} ${disabled === true ? 'disabled' : ''}`,
-        // ]"
-        // onClick="handleClick"
+        type="button"
+        class={`mmui__button mmui__button--${this.color} mmui__button--type-${this.type}-${this.color}`}
+        disabled={this.disabled}
+        onClick={handleClick}
       >
-        {renderSlot(this.$slot, 'default')}
+        {renderSlot(this.$slots, 'default')}
       </button>
     );
   },
 });
 
-export default MMUI_Button;
+export default MermaidUIButton;
