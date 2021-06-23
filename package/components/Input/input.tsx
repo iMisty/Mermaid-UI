e2ecfb7386cf4f9338ef67df4f012d3f0bd2a2b8
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2021-06-24 00:43:46 +0800

    Update: Demerge props

--- package/components/Input/input.tsx
+++ package/components/Input/input.tsx
@@ -1,12 +1,13 @@
 /*
  * @Author: Miya
  * @Date: 2021-05-06 07:55:34
- * @LastEditTime: 2021-05-26 10:38:12
+ * @LastEditTime: 2021-06-24 00:42:22
  * @LastEditors: Miya
  * @Description: Input Component
- * @FilePath: \Mermaid-UI\package\input\input.tsx
+ * @FilePath: \Mermaid-UI\package\components\Input\input.tsx
  */
 import { defineComponent, SetupContext } from 'vue';
+import { props } from './prop';
 import './input.less';
 
 type HTMLElementEvent<T extends HTMLElement> = Event & {
@@ -31,49 +32,8 @@ enum Grid {
 }
 
 const MermaidUIInput = defineComponent({
-  props: {
-    // 输入框标题
-    title: {
-      type: String,
-      default: '示例标题',
-    },
-    // 文本框类型
-    type: {
-      type: String,
-      default: 'text',
-    },
-    // 文本框大小
-    size: {
-      type: String,
-      default: 'normal',
-    },
-    // 是否并排
-    grid: {
-      type: String,
-      default: 'single',
-    },
-    // 底部提示文字
-    placeholder: {
-      type: String,
-      default: 'placeholder',
-    },
-    // 文本框文本
-    modelValue: {
-      type: String,
-      value: '',
-    },
-    // 文字数目限制
-    maxlength: {
-      type: Number,
-      default: 140,
-    },
-    // 提示文字
-    tips: {
-      type: String,
-      default: '111',
-    },
-  },
   emits: ['update:modelValue'],
+  props,
   setup(props, ctx: SetupContext) {
     // Emit: 修改操作
     const handleChange = ($event?: HTMLElementEvent<HTMLTextAreaElement>) => {
