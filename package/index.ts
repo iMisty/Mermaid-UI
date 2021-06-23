0f7907fb512174307684ebe72547b142fc19372d
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2021-06-23 23:49:38 +0800

    Create: Main

---
+++ package/index.ts
@@ -0,0 +1,13 @@
+/*
+ * @Author: Miya
+ * @Date: 2021-04-20 21:11:41
+ * @LastEditTime: 2021-06-23 23:44:47
+ * @LastEditors: Miya
+ * @Description: Main TS
+ * @FilePath: \Mermaid-UI\package\index.ts
+ */
+import Button from './components/Button/button';
+import Input from './components/Input/input';
+import Toast from './components/Toast/toast';
+
+export { Button, Input, Toast };
