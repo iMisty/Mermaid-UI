b52c77efd6399af803e14ee2e0395c862b137b9f
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2021-07-04 23:10:09 +0800

    Create layout.tsx

---
+++ package/components/Layout/layout.tsx
@@ -0,0 +1,18 @@
+/*
+ * @Author: Miya
+ * @Date: 2021-06-24 00:08:30
+ * @LastEditTime: 2021-06-29 23:33:12
+ * @LastEditors: Miya
+ * @Description: Layout Component
+ * @FilePath: \Mermaid-UI\package\components\Layout\layout.tsx
+ */
+import { defineComponent } from 'vue';
+
+const MermaidUILayout = defineComponent({
+  name:'MermaidUILayout',
+  setup(props, ctx) {},
+  render() {
+    return <div class="mmui__layout"></div>;
+  },
+});
+export default MermaidUILayout;
