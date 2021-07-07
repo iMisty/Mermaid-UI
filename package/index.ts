8af4149cde18327ca48fd8011d4671b603e75ecf
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2021-07-07 23:10:39 +0800

    Update index.ts

--- package/index.ts
+++ package/index.ts
@@ -1,14 +1,15 @@
 /*
  * @Author: Miya
  * @Date: 2021-04-20 21:11:41
- * @LastEditTime: 2021-06-24 00:04:02
+ * @LastEditTime: 2021-07-07 22:26:51
  * @LastEditors: Miya
  * @Description: Main TS
  * @FilePath: \Mermaid-UI\package\index.ts
  */
-import Button from './components/Button/button';
+import Button from './components/Button/index';
 import Card from './components/Card/card';
 import Input from './components/Input/input';
 import Toast from './components/Toast/toast';
 
 export { Button, Card, Input, Toast };
+// export { Button };
