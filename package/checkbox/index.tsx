7b825c19a0a6989e46ce0db495793d41b0c7db8f
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2021-07-16 00:08:57 +0800

    Create index.tsx

---
+++ package/checkbox/index.tsx
@@ -0,0 +1,20 @@
+/*
+ * @Author: Miya
+ * @Date: 2021-07-19 00:03:39
+ * @LastEditTime: 2021-07-19 00:04:15
+ * @LastEditors: Miya
+ * @Description: Checkbox Components
+ * @FilePath: \Mermaid-UI\package\checkbox\index.tsx
+ */
+import { defineComponent, reactive } from 'vue';
+const data = reactive({});
+
+const MermaidUICheckbox = defineComponent({
+  setup(props, ctx) {
+    return { data };
+  },
+  render() {
+    return <div class=""></div>;
+  },
+});
+export default MermaidUICheckbox;
