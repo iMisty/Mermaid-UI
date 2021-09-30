821aa5f07ff077f0c4ab1d97d7dc364ed939e9e7
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2021-10-01 04:36:16 +0800

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,26 @@
 <!--
  * @Author: Miya
  * @Date: 2021-09-25 20:54:39
- * @LastEditTime: 2021-09-25 23:15:48
+ * @LastEditTime: 2021-10-01 04:34:31
  * @LastEditors: Miya
  * @Description: d
- * @FilePath: \mermaidui2\src\views\Home.vue
+ * @FilePath: \MermaidUI\src\views\Home.vue
 -->
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  setup() {
+    const a = (b: any) => {
+      console.log(b);
+    };
+    return { a };
+  },
+});
+</script>
